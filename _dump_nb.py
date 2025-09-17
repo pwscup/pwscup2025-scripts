a4796a39c5d3f05d0cab405541ca3b7d39b125ae
thaya-png <thaya@ism.ac.jp>
2025-09-17 14:11:57 +0900

    multi_attack.csv fixed

--- _dump_nb.py
+++
@@ -1,7 +0,0 @@
-﻿import json
-p = r'attack/multi_attack.ipynb'
-nb = json.load(open(p, 'r', encoding='utf-8'))
-for idx in (12,13):
-    cell = nb['cells'][idx]
-    print('--- CELL', idx, '---')
-    print(''.join(cell.get('source', [])))
--- _verify_outmap.py
+++
@@ -1,7 +0,0 @@
-﻿import json
-p = r'attack/multi_attack.ipynb'
-nb = json.load(open(p, 'r', encoding='utf-8'))
-for idx, cell in enumerate(nb.get('cells', [])):
-    if cell.get('cell_type')=='code' and 'attack/new_attackDi_Ci_greedy.py' in ''.join(cell.get('source', [])):
-        print('CELL', idx)
-        print(''.join(cell['source']))
